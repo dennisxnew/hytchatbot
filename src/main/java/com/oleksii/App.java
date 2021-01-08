3e79cb473422fbaac7d389e6fb6e09d97bcff09e
Dennis.Chen <dennis.chen@hy-tech.com.tw>
2021-01-08 19:09:55 +0800

    1. 調整build設定

--- src/main/java/com/oleksii/App.java
+++ src/main/java/com/oleksii/App.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+public class App{
 
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
   }
 
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(App.class);
-  }
+//  @Override
+//  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//    return application.sources(App.class);
+//  }
 }

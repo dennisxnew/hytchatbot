89d56b3227d0dbc3342cf2a5290239a55479f7cf
Dennis.Chen <dennis.chen@hy-tech.com.tw>
2021-01-11 11:45:22 +0800

    testV2

--- src/main/java/com/oleksii/controllers/BotMessagesHandler.java
+++ src/main/java/com/oleksii/controllers/BotMessagesHandler.java
@@ -55,7 +55,7 @@ public class BotMessagesHandler {
 
   @GetMapping("/test")
   public String test(){
-    log.info("test!20210111_v1");
+    log.info("test!20210111_v2");
     return "test";
   }
 }
